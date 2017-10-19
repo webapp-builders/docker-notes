a79d74f3aa80f6af4a1c1f4252bc42322be719b2
Ashok Modi <btmash@gmail.com>
2017-10-18 18:34:15 -0700

    Update app.rb for sinatra to connect to redis.

--- examples/ch5/sinatra/webapp/lib/app.rb
+++ examples/ch5/sinatra/webapp/lib/app.rb
@@ -10,6 +10,10 @@ class App < Sinatra::Application
     "<h1>DockerBook Test Sinatra app</h1>"
   end
 
+  get '/webapp' do
+    "<h2>This is the webapp builders group</h2>"
+  end
+
   post '/json/?' do
     params.to_json
   end
